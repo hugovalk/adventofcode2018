240aa12cd7778e23883e92130ecb61525643472a
Hugo Valk <hugo.valk@gmail.com>
2018-12-05 22:52:37 +0100

    Parse input of exercise 3.

---
+++ src/clj/day3/ex3.clj
@@ -0,0 +1,22 @@
+(ns day3.ex3
+  (:require [clojure.java.io :as io]))
+
+(defn parse-line [line]
+  (->>
+    (re-matches #"^.* @ ([0-9]+),([0-9]+).*([0-9]+)x([0-9]+).*$" line)
+    (rest)
+    (map (fn [s] (Integer/parseInt s)))))
+
+(defn get-lines []
+  (with-open [rdr (io/reader "./src/clj/day3/input.txt")]
+    (->>
+      (line-seq rdr)
+      (map parse-line)
+      (doall))))
+
+(defn multiple-claimed-inches [])
+
+(defn -main []
+  (let [lines (get-lines)]
+    (println lines)
+    (println "The # of square inches having multiple claims: " (multiple-claimed-inches))))
