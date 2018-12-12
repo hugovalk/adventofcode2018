849920277f0baad26c57ee5cebbfb5fb5d2575f7
Hugo Valk <hugo.valk@gmail.com>
2018-12-12 22:20:19 +0100

    Full solution for day 5.

--- src/clj/day5/ex5.clj
+++ src/clj/day5/ex5.clj
@@ -5,13 +5,26 @@
   (with-open [rdr (io/reader "./input.txt")]
     (first (line-seq rdr))))
 
+(defn differs-case? [a b]
+  (= 32 (Math/abs (- (int a) (int b)))))
+
 (defn react [res next]
-  (if (= 32 (Math/abs
-              (- (int next)
-                 (int (first res)))))
-    (rest res)
-    (conj res next)))
+  (cond
+    (nil? (first res)) (conj res next)
+    (differs-case? next (first res)) (rest res)
+    :else (conj res next)))
+
+(defn counts-optimized [sequence]
+  (let [types (distinct (.toLowerCase sequence))]
+    (->> (map (fn [t]
+                (filter (fn [c] (not= (Character/toLowerCase c) t)) sequence)) types)
+         (map (fn [s] (reduce react () s)))
+         (map (fn [s] (count s)))
+         (map vector types))))
 
 (let [sequence (get-lines)]
   (println sequence)
-  (println (reduce react () sequence)))
+  (println "number of units: " (count sequence))
+  (println "number of units left after reaction: " (count (reduce react () sequence)))
+  (println "number of units left after reaction for optimized polymers: "
+           (sort-by second < (counts-optimized sequence))))
