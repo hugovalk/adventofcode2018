3353cfdf2d5a80675a119d7f6499abd2797a556d
Hugo Valk <hugo.valk@gmail.com>
2018-12-17 23:37:17 +0100

    Implement Manhattan distances.

--- src/clj/day6/ex6.clj
+++ src/clj/day6/ex6.clj
@@ -1,5 +1,15 @@
 (ns day6.ex6
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:import (java.util Date)))
+
+(defn abs [a b]
+  (Math/abs (- a b)))
+
+(defn indices [coll]
+  (range (count coll)))
+
+(defn zip-with-index [coll]
+  (map vector coll (indices coll)))
 
 (defn get-lines []
   (with-open [rdr (io/reader "./input.txt")]
@@ -9,26 +19,35 @@
                      (vec)
                      (map (fn [i] (Integer/parseInt (.trim i))))))))))
 
-(defn man-dis [a b]
-  (+
-    (Math/abs (- (first a) (first b)))
-    (Math/abs (- (second a) (second b)))))
-
 (defn max-xy [lines]
   (reduce (fn [res l]
             [(max (first res) (first l))
              (max (second res) (second l))])
           [0 0] lines))
 
-(defn world [max]
-  (vec (map (fn [_] (vec (repeat (first max) -1)))
-            (repeat (second max) []))))
+(defn init-world [max]
+  (vec (map (fn [_] (vec (repeat (+ 1 (first max)) -1)))
+            (repeat (+ 1 (second max)) []))))
 
 (defn print-world [world]
   (doseq [w world]
     (println w)))
 
+(defn manhattan-distances [loc world]
+  (let [rows-i (zip-with-index world)]
+    (reduce (fn [row-res row-i]
+              (conj row-res
+                    (map (fn [c] (+ c (abs (second loc) (second row-i))))
+                         (reduce (fn [col-res col]
+                                   (conj col-res (abs (first loc) col)))
+                                 [] (indices (first row-i))))))
+            [] rows-i)))
+
+(def start (.getTime (Date.)))
 (let [lines (get-lines)
-      locs (map vector (range (count lines)) lines)
-      max (max-xy lines)]
-  (print-world (world max)))
+      locs (zip-with-index lines)
+      max (max-xy lines)
+      world (init-world max)
+      locs-dista (map (fn [loc] manhattan-distances loc world) locs)]
+  (println (count locs-dista))
+  (println "Done in" (- (.getTime (Date.)) start) "ms"))
